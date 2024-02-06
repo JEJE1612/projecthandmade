ab040d624b402d41bfd51027a2da00f6394f57fc
Ahmed abdelnadi <93190015+am7405782@users.noreply.github.com>
2024-02-06 07:19:44 +0200

    app SomeBackage

---
+++ lib/feather/pages/Auth/mangment/LoginState.dart
@@ -0,0 +1,22 @@
+abstract class LoginState {}
+
+class InitalState extends LoginState {}
+
+class ChangeIconSuffix extends LoginState {}
+
+class LodingLoginState extends LoginState {}
+
+class LoginSucssesState extends LoginState {
+  final String uid;
+  LoginSucssesState({
+    required this.uid,
+  });
+}
+
+class LoginFailureState extends LoginState {}
+
+class Forgetpasswordstate extends LoginState {}
+
+class LoginAsClint extends LoginState {}
+
+class LoginAsConsltent extends LoginState {}
