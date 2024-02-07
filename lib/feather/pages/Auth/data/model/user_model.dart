87b285cf2ff412f2cbfc6e1aae1e2e35eecb6b8f
Ahmed abdelnadi <93190015+am7405782@users.noreply.github.com>
2024-02-07 12:07:11 +0200

    user_model

--- lib/feather/pages/Auth/data/model/user_model.dart
+++ lib/feather/pages/Auth/data/model/user_model.dart
@@ -1,80 +1,34 @@
-import 'dart:convert';
-
-class LoginModel {
-  String? email;
+class UserModel {
   String? name;
-  bool? emailVerified;
-  DateTime? tokenExpiration;
-  String? jwtToken;
-  String? role;
-  String? profileImage;
-
-  LoginModel({
-    this.email,
-    this.name,
-    this.emailVerified,
-    this.tokenExpiration,
-    this.jwtToken,
-    this.role,
-    this.profileImage,
+  String? email;
+  String? phone;
+  String? uid;
+  String? type;
+  String? image;
+  UserModel({
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.uid,
+    required this.type,
+    required this.image,
   });
-
-  @override
-  String toString() {
-    return 'LoginModel(email: $email, name: $name, emailVerified: $emailVerified, tokenExpiration: $tokenExpiration, jwtToken: $jwtToken, role: $role, profileImage: $profileImage)';
+  UserModel.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    email = json['email'];
+    phone = json['phone'];
+    uid = json['uid'];
+    type = json['type'];
+    image = json['image'];
   }
-
-  factory LoginModel.fromMap(Map<String, dynamic> data) => LoginModel(
-        email: data['Email'] as String?,
-        name: data['Name'] as String?,
-        emailVerified: data['EmailVerified'] as bool?,
-        tokenExpiration: data['TokenExpiration'] == null
-            ? null
-            : DateTime.parse(data['TokenExpiration'] as String),
-        jwtToken: data['JWTToken'] as String?,
-        role: data['Role'] as String?,
-        profileImage: data['ProfileImage'] as String?,
-      );
-
-  Map<String, dynamic> toMap() => {
-        'Email': email,
-        'Name': name,
-        'EmailVerified': emailVerified,
-        'TokenExpiration': tokenExpiration?.toIso8601String(),
-        'JWTToken': jwtToken,
-        'Role': role,
-        'ProfileImage': profileImage,
-      };
-
-  /// `dart:convert`
-  ///
-  /// Parses the string and returns the resulting Json object as [LoginModel].
-  factory LoginModel.fromJson(String data) {
-    return LoginModel.fromMap(json.decode(data) as Map<String, dynamic>);
-  }
-
-  /// `dart:convert`
-  ///
-  /// Converts [LoginModel] to a JSON string.
-  String toJson() => json.encode(toMap());
-
-  LoginModel copyWith({
-    String? email,
-    String? name,
-    bool? emailVerified,
-    DateTime? tokenExpiration,
-    String? jwtToken,
-    String? role,
-    String? profileImage,
-  }) {
-    return LoginModel(
-      email: email ?? this.email,
-      name: name ?? this.name,
-      emailVerified: emailVerified ?? this.emailVerified,
-      tokenExpiration: tokenExpiration ?? this.tokenExpiration,
-      jwtToken: jwtToken ?? this.jwtToken,
-      role: role ?? this.role,
-      profileImage: profileImage ?? this.profileImage,
-    );
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'email': email,
+      'phone': phone,
+      'uid': uid,
+      'type': type,
+      'image': image,
+    };
   }
 }
--- lib/feather/pages/Auth/mangment/bloc_register/register_blocs.dart
+++ lib/feather/pages/Auth/mangment/bloc_register/register_blocs.dart
@@ -1,12 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:handmade/feather/pages/Auth/data/model/user_model.dart';
 import 'package:handmade/feather/pages/Auth/mangment/bloc_register/register_state.dart';
 
 class CreatAccount extends Cubit<CratAccountState> {
   CreatAccount() : super(InitalState());
   static CreatAccount get(context) => BlocProvider.of(context);
-  // String type = "client";
-  // bool isClient = true;
+  String type = "client";
+  bool isClient = true;
   TextEditingController email = TextEditingController();
 
   TextEditingController password = TextEditingController();
@@ -16,47 +19,45 @@ class CreatAccount extends Cubit<CratAccountState> {
   TextEditingController phone = TextEditingController();
 
   var formkey = GlobalKey<FormState>();
-  // changeIsAdmin() {
-  //   isClient = !isClient;
-  //   emit(ChangetypeState());
-  // }
 
-  // Future<void> userRegister() async {
-  //   emit(CreatUserLoadingState());
-  //   try {
-  //     await FirebaseAuth.instance.createUserWithEmailAndPassword(
-  //       email: email.text,
-  //       password: password.text,
-  //     );
-  //     creatSaveData();
-  //     emit(ScafullCreatUserState());
-  //   } catch (e) {
-  //     emit(ErrorCrestLodingState(error: "Error check your data"));
-  //   }
-  // }
+  changeIsAdmin() {
+    isClient = !isClient;
+    emit(ChangetypeState());
+  }
 
-  // Future<void> creatSaveData() async {
-  //   UserModel? model = UserModel(
-  //     type: type,
-  //     email: email.text,
-  //     name: name.text,
-  //     phone: phone.text,
-  //     uid: FirebaseAuth.instance.currentUser!.uid,
-  //     bio: "writebio.",
-  //     cover:
-  //         "https://images.pexels.com/photos/268941/pexels-photo-268941.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  //     image:
-  //         "https://images.pexels.com/photos/5588224/pexels-photo-5588224.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  //   );
-  //   emit(SaveDataFireStoreLoding());
-  //   try {
-  //     await FirebaseFirestore.instance
-  //         .collection("user")
-  //         .doc(FirebaseAuth.instance.currentUser!.uid)
-  //         .set(model.toMap());
-  //     emit(SaveDataFireStoreScafull());
-  //   } catch (e) {
-  //     emit(SaveDataFireStoreerror(error: e.toString()));
-  //   }
-  // }
+  Future<void> userRegister() async {
+    emit(CreatUserLoadingState());
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email.text,
+        password: password.text,
+      );
+      creatSaveData();
+      emit(ScafullCreatUserState());
+    } catch (e) {
+      emit(ErrorCrestLodingState(error: "Error check your data"));
+    }
+  }
+
+  Future<void> creatSaveData() async {
+    UserModel? model = UserModel(
+      type: type,
+      email: email.text,
+      name: name.text,
+      phone: phone.text,
+      uid: FirebaseAuth.instance.currentUser!.uid,
+      image:
+          "https://www.behance.net/gallery/148207527/3D-Avatars/modules/837188661",
+    );
+    emit(SaveDataFireStoreLoding());
+    try {
+      await FirebaseFirestore.instance
+          .collection("user")
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .set(model.toMap());
+      emit(SaveDataFireStoreScafull());
+    } catch (e) {
+      emit(SaveDataFireStoreerror(error: e.toString()));
+    }
+  }
 }
