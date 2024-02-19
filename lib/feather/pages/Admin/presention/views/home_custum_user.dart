f049f0f96d67f579b2804a588aca88640f622f8c
Ahmed abdelnadi <93190015+am7405782@users.noreply.github.com>
2024-02-19 02:25:42 +0200

    finah home

--- lib/feather/pages/Admin/presention/views/home_custum_user.dart
+++ lib/feather/pages/Admin/presention/views/home_custum_user.dart
@@ -1,8 +1,13 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
+import 'package:handmade/cors/theme/colors.dart';
+import 'package:handmade/cors/theme/style_text.dart';
+import 'package:handmade/feather/pages/Admin/data/model/prodect_model.dart';
 import 'package:handmade/feather/pages/Admin/mangment/prodect/prodect_bloc.dart';
 import 'package:handmade/feather/pages/Admin/mangment/prodect/prodect_state.dart';
+import 'package:handmade/feather/pages/Admin/presention/Home_owner/views/prodect_deatils.dart';
 import 'package:handmade/feather/pages/Admin/presention/widgets/show_catg/views/show_cat.dart';
 import 'package:handmade/feather/user/management/bloc.dart';
 import 'package:handmade/feather/user/management/my_state.dart';
@@ -43,6 +48,7 @@ class HomeCustomUser extends StatelessWidget {
               height: MediaQuery.of(context).size.height * 0.19,
               child: const ListProdect()),
         ),
+        Gap(20),
       ]),
     );
   }
@@ -86,14 +92,99 @@ class ListProdect extends StatelessWidget {
           scrollDirection: Axis.horizontal,
           itemCount: ProdectBloc.get(context).prodects.length,
           itemBuilder: (context, index) {
-            return Container(
-              color: Colors.amberAccent,
-              height: 100,
-              width: 50,
+            return CardItemsProdectUser(
+              prodect: ProdectBloc.get(context).prodects[index],
             );
           },
         );
       },
+    );
+  }
+}
+
+class CardItemsProdectUser extends StatelessWidget {
+  const CardItemsProdectUser({
+    super.key,
+    required this.prodect,
+  });
+  final ProdectModel prodect;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ProdectDeatils(
+                prodect: prodect,
+              ),
+            ));
+      },
+      child: Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: CachedNetworkImageProvider(
+                      prodect.prodectImage ?? "",
+                    ),
+                  ),
+                ),
+                width: MediaQuery.of(context).size.width * 0.45,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 10,
+                right: 2,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              prodect.price ?? "",
+                              style:
+                                  Styles.textStyle16.copyWith(color: primary),
+                            ),
+                            const Icon(
+                              Icons.attach_money,
+                              color: secondary,
+                            ),
+                          ],
+                        ),
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.favorite,
+                              color: Colors.red,
+                              size: 25,
+                            ))
+                      ],
+                    ),
+                  ),
+                  Text(
+                    prodect.prodectname ?? "",
+                    style: Styles.textStyle16,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
